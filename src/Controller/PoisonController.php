a9b2e7a73ab74dc5f7e721a0cdf548b12dda8b14
ManonMarizy <manon.marizy@gmail.com>
2020-11-20 01:38:59 +0100

    debut pour affichage fiche poison

--- src/Controller/PoisonController.php
+++ src/Controller/PoisonController.php
@@ -3,8 +3,25 @@
 namespace App\Controller;
 
 use App\Model\PoisonManager;
+use App\Model\CureManager;
 
 class PoisonController extends AbstractController
 {
-
+    public function show(int $getId)
+    {
+        $getById = filter_var($getId, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1]]);
+        $cureManager = new CureManager();
+        $poisonManager = new PoisonManager();
+        $cureInformations = $cureManager->selectOneCureById($getById);
+        $poisonInformations = $poisonManager->selectOneById($getById);
+        $poisonSymptoms = $poisonManager->selectSymptomsById($getId);
+        if (!isset($cureInformations['igredient'])) {
+            header("Location: /");
+        }
+        return $this->twig->render('CureManager/show.html.twig', [
+            'cureInformations' => $cureInformations,
+            'poisonInformations' => $poisonInformations,
+            'poisonSymptoms' => $poisonSymptoms
+        ]);
+    }
 }
--- src/Model/PoisonManager.php
+++ src/Model/PoisonManager.php
@@ -7,8 +7,7 @@ class PoisonManager extends AbstractManager
     /**
      *
      */
-    const TABLE = '';
-    //TODO:mettre le bon nom de table
+    const TABLE = 'poison';
 
     /**
      *  Initializes this class.
@@ -16,5 +15,19 @@ class PoisonManager extends AbstractManager
     public function __construct()
     {
         parent::__construct(self::TABLE);
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function selectSymptomsById(int $id)
+    {
+        $query = "SELECT * FROM symptom JOIN poison_has_symptom phs ON poison.id = phs.poison_id
+        WHERE poison_id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $id, \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetchAll();
     }
 }
